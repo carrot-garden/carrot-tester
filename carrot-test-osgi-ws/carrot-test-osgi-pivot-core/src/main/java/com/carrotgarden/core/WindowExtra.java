e778da454273e6e08e69f627df27e0446e23a436
Andrei.Pozolotin <Andrei.Pozolotin@gmail.com>
2011-05-16 17:33:02 -0500

    a

--- carrot-test-osgi-ws/carrot-test-osgi-pivot-core/src/main/java/com/carrotgarden/core/WindowExtra.java
+++ carrot-test-osgi-ws/carrot-test-osgi-pivot-core/src/main/java/com/carrotgarden/core/WindowExtra.java
@@ -4,25 +4,14 @@ import java.net.URL;
 
 import org.apache.pivot.beans.Bindable;
 import org.apache.pivot.collections.Map;
-import org.apache.pivot.collections.Sequence;
 import org.apache.pivot.util.Resources;
-import org.apache.pivot.util.concurrent.TaskExecutionException;
-import org.apache.pivot.wtk.Alert;
-import org.apache.pivot.wtk.ApplicationContext;
 import org.apache.pivot.wtk.Button;
-import org.apache.pivot.wtk.ButtonGroup;
-import org.apache.pivot.wtk.ButtonPressListener;
-import org.apache.pivot.wtk.ImageView;
-import org.apache.pivot.wtk.Label;
-import org.apache.pivot.wtk.ListButton;
-import org.apache.pivot.wtk.ListButtonSelectionListener;
-import org.apache.pivot.wtk.ListView;
-import org.apache.pivot.wtk.ListViewSelectionListener;
-import org.apache.pivot.wtk.MessageType;
-import org.apache.pivot.wtk.PushButton;
-import org.apache.pivot.wtk.Span;
+import org.apache.pivot.wtk.ButtonStateListener;
+import org.apache.pivot.wtk.Checkbox;
+import org.apache.pivot.wtk.FlowPane;
+import org.apache.pivot.wtk.HorizontalAlignment;
+import org.apache.pivot.wtk.RadioButton;
 import org.apache.pivot.wtk.Window;
-import org.apache.pivot.wtk.media.Image;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,172 +20,61 @@ public class WindowExtra extends Window implements Bindable {
 	protected static final Logger log = LoggerFactory
 			.getLogger(WindowExtra.class);
 
-	private PushButton pushButton;
-
-	private PushButton selectButton;
-
-	private ListView listView;
-	private Label selectionLabel;
-
-	private ListButton listButton;
-	private ImageView imageView;
-
 	@Override
 	public void initialize(Map<String, Object> namespace, URL location,
 			Resources resources) {
 
-		// ###############
+		log.info("init");
 
-		pushButton = (PushButton) namespace.get("pushButton");
+		flowPane = (FlowPane) namespace.get("flowPane");
+		leftRadioButton = (RadioButton) namespace.get("leftRadioButton");
+		rightRadioButton = (RadioButton) namespace.get("rightRadioButton");
+		centerRadioButton = (RadioButton) namespace.get("centerRadioButton");
+		alignToBaselineCheckbox = (Checkbox) namespace
+				.get("alignToBaselineCheckbox");
 
-		ButtonPressListener listener = new ButtonPressListener() {
+		ButtonStateListener buttonStateListener = new ButtonStateListener() {
 			@Override
-			public void buttonPressed(Button button) {
-				Alert.alert(MessageType.INFO, "You clicked me!",
-						WindowExtra.this);
+			public void stateChanged(Button button, Button.State previousState) {
+				updateFlowPaneState();
 			}
 		};
 
-		pushButton.getButtonPressListeners().add(listener);
+		leftRadioButton.getButtonStateListeners().add(buttonStateListener);
+		rightRadioButton.getButtonStateListeners().add(buttonStateListener);
+		centerRadioButton.getButtonStateListeners().add(buttonStateListener);
+		alignToBaselineCheckbox.getButtonStateListeners().add(
+				buttonStateListener);
 
-		// ###############
-
-		selectButton = (PushButton) namespace.get("selectButton");
-
-		// Get a reference to the button group
-		final ButtonGroup numbersGroup = (ButtonGroup) namespace.get("numbers");
-
-		// Add a button press listener
-		selectButton.getButtonPressListeners().add(new ButtonPressListener() {
-			@Override
-			public void buttonPressed(Button button) {
-				String message = "You selected \""
-						+ numbersGroup.getSelection().getButtonData() + "\".";
-				Alert.alert(MessageType.INFO, message, WindowExtra.this);
-			}
-		});
-
-		// ###############
-
-		selectionLabel = (Label) namespace.get("selectionLabel");
-		listView = (ListView) namespace.get("listView");
-
-		listView.getListViewSelectionListeners().add(
-				new ListViewSelectionListener() {
-
-					@Override
-					public void selectedRangeAdded(ListView listView,
-							int rangeStart, int rangeEnd) {
-						updateSelection(listView);
-					}
-
-					@Override
-					public void selectedRangeRemoved(ListView listView,
-							int rangeStart, int rangeEnd) {
-						updateSelection(listView);
-					}
-
-					@Override
-					public void selectedRangesChanged(ListView listView,
-							Sequence<Span> previousSelectedRanges) {
-						if (previousSelectedRanges != null
-								&& previousSelectedRanges != listView
-										.getSelectedRanges()) {
-							updateSelection(listView);
-						}
-					}
-
-					@Override
-					public void selectedItemChanged(ListView listView,
-							Object previousSelectedItem) {
-						// No-op
-					}
-
-					private void updateSelection(ListView listView) {
-
-						String selectionText = "";
-
-						Sequence<Span> selectedRanges = listView
-								.getSelectedRanges();
-
-						for (int i = 0, n = selectedRanges.getLength(); i < n; i++) {
-							Span selectedRange = selectedRanges.get(i);
-
-							for (int j = selectedRange.start; j <= selectedRange.end; j++) {
-								if (selectionText.length() > 0) {
-									selectionText += ", ";
-								}
-
-								String text = (String) listView.getListData()
-										.get(j);
-								selectionText += text;
-							}
-						}
-
-						selectionLabel.setText(selectionText);
-					}
-				});
-
-		// ###############
-
-		listButton = (ListButton) namespace.get("listButton");
-		imageView = (ImageView) namespace.get("imageView");
-
-		listButton.getListButtonSelectionListeners().add(
-				new ListButtonSelectionListener.Adapter() {
-
-					@Override
-					public void selectedItemChanged(ListButton listButton,
-							Object previousSelectedItem) {
-
-						Object selectedItem = listButton.getSelectedItem();
-
-						if (selectedItem != null) {
-
-							log.info("XXX selectedItem : {}", selectedItem);
-
-							// Get the image URL for the selected item
-							// ClassLoader classLoader = Thread.currentThread()
-							// .getContextClassLoader();
-							ClassLoader classLoader = getClass()
-									.getClassLoader();
-
-							log.info("XXX classLoader : {}", classLoader);
-
-							// XXX
-							URL imageURL = classLoader.getResource("/tutorial/"
-									+ selectedItem);
-
-							log.info("XXX imageURL : {}", imageURL);
-
-							/*
-							 * If the image has not been added to the resource
-							 * cache yet, add it
-							 */
-							Image image = (Image) ApplicationContext
-									.getResourceCache().get(imageURL);
-
-							if (image == null) {
-								try {
-									image = Image.load(imageURL);
-								} catch (TaskExecutionException exception) {
-									throw new RuntimeException(exception);
-								}
-
-								ApplicationContext.getResourceCache().put(
-										imageURL, image);
-							}
-
-							// Update the image
-							imageView.setImage(image);
-						}
-					}
-				});
-
-		listButton.setSelectedIndex(0);
+		updateFlowPaneState();
 
 	}
 
-	// ###############
+	private void updateFlowPaneState() {
+		HorizontalAlignment alignment = null;
+
+		if (leftRadioButton.isSelected()) {
+			alignment = HorizontalAlignment.LEFT;
+		} else if (rightRadioButton.isSelected()) {
+			alignment = HorizontalAlignment.RIGHT;
+		} else if (centerRadioButton.isSelected()) {
+			alignment = HorizontalAlignment.CENTER;
+		}
+
+		if (alignment != null) {
+			flowPane.getStyles().put("alignment", alignment);
+		}
+
+		flowPane.getStyles().put("alignToBaseline",
+				alignToBaselineCheckbox.isSelected());
+	}
+
+	private FlowPane flowPane;
+	private RadioButton leftRadioButton;
+	private RadioButton rightRadioButton;
+	private RadioButton centerRadioButton;
+	private Checkbox alignToBaselineCheckbox;
+
+	// ####################
 
 }
